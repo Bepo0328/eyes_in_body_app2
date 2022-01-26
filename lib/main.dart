30f33147e4beb90041edefd342aea94897664c29
Bepo0328 <82445568+Bepo0328@users.noreply.github.com>
2022-01-26 22:46:57 +0900

    EyesInBodyApp2: Notification Settings

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,34 @@
 import 'package:eyes_in_body_app2/data/data.dart';
 import 'package:eyes_in_body_app2/data/database.dart';
+import 'package:eyes_in_body_app2/style.dart';
 import 'package:eyes_in_body_app2/utils.dart';
 import 'package:eyes_in_body_app2/view/body.dart';
 import 'package:eyes_in_body_app2/view/food.dart';
 import 'package:eyes_in_body_app2/view/workout.dart';
 import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:intl/intl.dart';
 import 'package:table_calendar/table_calendar.dart';
+import 'package:timezone/timezone.dart' as tz;
+import 'package:timezone/data/latest.dart' as tz;
 
-import 'style.dart';
+FlutterLocalNotificationsPlugin? flutterLocalNotificationsPlugin;
 
-void main() {
+void main() async {
   runApp(const MyApp());
+
+  tz.initializeTimeZones();
+
+  const AndroidNotificationChannel androidNotificationChannel =
+      AndroidNotificationChannel('bepo', 'Diet App', description: 'Diet App');
+  flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
+
+  await flutterLocalNotificationsPlugin!
+      .resolvePlatformSpecificImplementation<
+          AndroidFlutterLocalNotificationsPlugin>()
+      ?.createNotificationChannel(androidNotificationChannel);
 }
 
 class MyApp extends StatelessWidget {
@@ -51,22 +66,48 @@ class _MyHomePageState extends State<MyHomePage> {
   DateTime dateTime = DateTime.now();
 
   List<Food> foods = [];
+  List<Food> allFoods = [];
   List<Workout> workouts = [];
+  List<Workout> allWorkouts = [];
   List<EyeBody> eyeBodies = [];
-  List<Weight> weight = [];
+  List<EyeBody> allEyeBodies = [];
   List<Weight> weights = [];
+  List<Weight> allWeights = [];
+
+  // 알림 초기화 및 알림 설정
+  Future<bool> initNotification() async {
+    flutterLocalNotificationsPlugin ??= FlutterLocalNotificationsPlugin();
+
+    var initSettingAndroid = const AndroidInitializationSettings('app_icon');
+    var initSettingIOS = const IOSInitializationSettings();
+
+    var initSetting = InitializationSettings(
+      android: initSettingAndroid,
+      iOS: initSettingIOS,
+    );
+
+    await flutterLocalNotificationsPlugin!
+        .initialize(initSetting, onSelectNotification: (payload) async {});
+
+    setScheduling();
+
+    return true;
+  }
 
   void getHistories() async {
     int _d = Utils.getFormTime(dateTime);
 
     foods = await dbHelper.queryFoodByDate(_d);
+    allFoods = await dbHelper.queryAllFood();
     workouts = await dbHelper.queryWorkoutByDate(_d);
+    allWorkouts = await dbHelper.queryAllWorkout();
     eyeBodies = await dbHelper.queryEyeBodyByDate(_d);
-    weight = await dbHelper.queryWeightByDate(_d);
-    weights = await dbHelper.queryAllWeight();
+    allEyeBodies = await dbHelper.queryAllEyeBody();
+    weights = await dbHelper.queryWeightByDate(_d);
+    allWeights = await dbHelper.queryAllWeight();
 
-    if (weight.isNotEmpty) {
-      final w = weight.first;
+    if (weights.isNotEmpty) {
+      final w = weights.first;
       wCtrl.text = w.weight.toString();
       mCtrl.text = w.muscle.toString();
       fCtrl.text = w.fat.toString();
@@ -82,7 +123,35 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   void initState() {
     getHistories();
+    initNotification();
     super.initState();
+  }
+
+  void setScheduling() {
+    var android = const AndroidNotificationDetails('bepo', 'Diet App',
+        channelDescription: 'Diet App',
+        importance: Importance.max,
+        priority: Priority.max);
+
+    var iOS = IOSNotificationDetails();
+
+    NotificationDetails detail = NotificationDetails(
+      android: android,
+      iOS: iOS,
+    );
+
+    flutterLocalNotificationsPlugin!.zonedSchedule(
+      0,
+      '오늘의 다이어트를 기록해주세요!',
+      '앱을 실행해주세요!',
+      tz.TZDateTime.from(DateTime.now().add(const Duration(seconds: 10)), tz.local),
+      detail,
+      uiLocalNotificationDateInterpretation:
+          UILocalNotificationDateInterpretation.absoluteTime,
+      androidAllowWhileIdle: true,
+      payload: 'Diet App',
+      matchDateTimeComponents: DateTimeComponents.time,
+    );
   }
 
   @override
@@ -215,6 +284,8 @@ class _MyHomePageState extends State<MyHomePage> {
       return getHistoryWidget();
     } else if (currentIndex == 2) {
       return getWeightWidget();
+    } else if (currentIndex == 3) {
+      return getStatisticWidget();
     } else {
       return Container();
     }
@@ -276,10 +347,10 @@ class _MyHomePageState extends State<MyHomePage> {
                 if (idx == 0) {
                   // 몸무게
 
-                  if (weight.isEmpty) {
+                  if (weights.isEmpty) {
                     return Container();
                   } else {
-                    final w = weight.first;
+                    final w = weights.first;
 
                     return Container(
                       height: cardSize,
@@ -511,7 +582,7 @@ class _MyHomePageState extends State<MyHomePage> {
                       InkWell(
                         onTap: () async {
                           Weight w;
-                          if (weight.isEmpty) {
+                          if (weights.isEmpty) {
                             w = Weight(
                               date: Utils.getFormTime(dateTime),
                               weight: 0,
@@ -519,7 +590,7 @@ class _MyHomePageState extends State<MyHomePage> {
                               muscle: 0,
                             );
                           } else {
-                            w = weight.first;
+                            w = weights.first;
                           }
 
                           w.weight = int.tryParse(wCtrl.text) ?? 0;
@@ -623,7 +694,7 @@ class _MyHomePageState extends State<MyHomePage> {
           } else if (idx == 2) {
             List<FlSpot> spots = [];
 
-            for (final w in weights) {
+            for (final w in allWeights) {
               if (chartIndex == 0) {
                 // 몸무게
                 spots.add(FlSpot(w.date.toDouble(), w.weight.toDouble()));
@@ -784,6 +855,223 @@ class _MyHomePageState extends State<MyHomePage> {
           }
         },
         itemCount: 3,
+      ),
+    );
+  }
+
+  Widget getStatisticWidget() {
+    return Container(
+      child: ListView.builder(
+        itemBuilder: (ctx, idx) {
+          if (idx == 0) {
+            List<FlSpot> spots = [];
+
+            for (final w in allWorkouts) {
+              if (chartIndex == 0) {
+                // 몸무게
+                spots.add(FlSpot(w.date.toDouble(), w.time.toDouble()));
+              } else if (chartIndex == 1) {
+                // 골격근
+                spots.add(FlSpot(w.date.toDouble(), w.kcal.toDouble()));
+              } else {
+                // 체지방
+                spots.add(FlSpot(w.date.toDouble(), w.distance.toDouble()));
+              }
+            }
+
+            return Container(
+              margin:
+                  const EdgeInsets.symmetric(horizontal: 16.0, vertical: 12.0),
+              child: Column(
+                children: [
+                  Row(
+                    children: [
+                      InkWell(
+                        onTap: () async {
+                          setState(() {
+                            chartIndex = 0;
+                          });
+                        },
+                        child: Container(
+                          padding: const EdgeInsets.symmetric(
+                            horizontal: 10.0,
+                            vertical: 6.0,
+                          ),
+                          child: Text(
+                            '운동 시간',
+                            style: TextStyle(
+                              color: chartIndex == 0 ? Colors.white : iTxtColor,
+                            ),
+                          ),
+                          decoration: BoxDecoration(
+                            color: chartIndex == 0 ? mainColor : iBgColor,
+                            borderRadius: BorderRadius.circular(8.0),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(width: 8.0),
+                      InkWell(
+                        onTap: () async {
+                          setState(() {
+                            chartIndex = 1;
+                          });
+                        },
+                        child: Container(
+                          padding: const EdgeInsets.symmetric(
+                            horizontal: 10.0,
+                            vertical: 6.0,
+                          ),
+                          child: Text(
+                            '칼로리',
+                            style: TextStyle(
+                              color: chartIndex == 1 ? Colors.white : iTxtColor,
+                            ),
+                          ),
+                          decoration: BoxDecoration(
+                            color: chartIndex == 1 ? mainColor : iBgColor,
+                            borderRadius: BorderRadius.circular(8.0),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(width: 8.0),
+                      InkWell(
+                        onTap: () async {
+                          setState(() {
+                            chartIndex = 2;
+                          });
+                        },
+                        child: Container(
+                          padding: const EdgeInsets.symmetric(
+                            horizontal: 10.0,
+                            vertical: 6.0,
+                          ),
+                          child: Text(
+                            '거리',
+                            style: TextStyle(
+                              color: chartIndex == 2 ? Colors.white : iTxtColor,
+                            ),
+                          ),
+                          decoration: BoxDecoration(
+                            color: chartIndex == 2 ? mainColor : iBgColor,
+                            borderRadius: BorderRadius.circular(8.0),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  Container(
+                    height: 300.0,
+                    margin: const EdgeInsets.symmetric(
+                      horizontal: 16.0,
+                      vertical: 16.0,
+                    ),
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 25.0,
+                      vertical: 16.0,
+                    ),
+                    decoration: BoxDecoration(
+                      color: bgColor,
+                      borderRadius: BorderRadius.circular(12.0),
+                      boxShadow: const [
+                        BoxShadow(
+                          blurRadius: 4.0,
+                          spreadRadius: 4.0,
+                          color: Colors.black12,
+                        ),
+                      ],
+                    ),
+                    child: LineChart(
+                      LineChartData(
+                        lineBarsData: [
+                          LineChartBarData(spots: spots, colors: [mainColor]),
+                        ],
+                        gridData: FlGridData(
+                          show: false,
+                        ),
+                        borderData: FlBorderData(
+                          show: false,
+                        ),
+                        lineTouchData: LineTouchData(
+                          touchTooltipData: LineTouchTooltipData(
+                            getTooltipItems: (spots) {
+                              return [
+                                LineTooltipItem(
+                                  '${spots.first.y}',
+                                  TextStyle(color: mainColor),
+                                ),
+                              ];
+                            },
+                          ),
+                        ),
+                        titlesData: FlTitlesData(
+                          bottomTitles: SideTitles(
+                            showTitles: true,
+                            getTitles: (value) {
+                              DateTime date = Utils.stringToDateTime(
+                                  value.toInt().toString());
+                              return '${date.day}일';
+                            },
+                          ),
+                          leftTitles: SideTitles(
+                            showTitles: false,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            );
+          } else if (idx == 1) {
+            return Container(
+              height: cardSize,
+              child: ListView.builder(
+                itemBuilder: (ctx, _idx) {
+                  return Container(
+                    height: cardSize,
+                    width: cardSize,
+                    child: MainFoodCard(food: allFoods[_idx]),
+                  );
+                },
+                itemCount: allFoods.length,
+                scrollDirection: Axis.horizontal,
+              ),
+            );
+          } else if (idx == 2) {
+            return Container(
+              height: cardSize,
+              child: ListView.builder(
+                itemBuilder: (ctx, _idx) {
+                  return Container(
+                    height: cardSize,
+                    width: cardSize,
+                    child: MainWorkout(workout: allWorkouts[_idx]),
+                  );
+                },
+                itemCount: allWorkouts.length,
+                scrollDirection: Axis.horizontal,
+              ),
+            );
+          } else if (idx == 3) {
+            return Container(
+              height: cardSize,
+              child: ListView.builder(
+                itemBuilder: (ctx, _idx) {
+                  return Container(
+                    height: cardSize,
+                    width: cardSize,
+                    child: MainEyeBodyCard(eyeBody: allEyeBodies[_idx]),
+                  );
+                },
+                itemCount: allEyeBodies.length,
+                scrollDirection: Axis.horizontal,
+              ),
+            );
+          } else {
+            return Container();
+          }
+        },
+        itemCount: 4,
       ),
     );
   }
